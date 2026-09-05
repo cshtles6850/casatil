a9f248bb8d0180707d759f30c690c7d91f107bb7
cappa681 <airportcappadociatransfer@gmail.com>
2026-09-06 01:27:00 +0300

    ..

--- app/app/(en)/not-found.tsx
+++
@@ -1,4 +0,0 @@
-import Link from 'next/link';
-export default function NotFound() {
-  return <main className="section"><div className="container"><h1>Page not found</h1><p className="lead">The transfer page you requested does not exist.</p><Link className="btn btn-primary" href="/">Back to homepage</Link></div></main>;
-}
